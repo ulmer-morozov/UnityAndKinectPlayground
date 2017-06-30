a3ceb538d448930b3fcaff9ac1eeaed9c64137e7
ulmer-morozov <ulmer.morozov@gmail.com>
2017-06-30 21:17:32 +0300

    Рефакторинг. Добавлено управление презетами наполнителей.

---
+++ Assets/FillerPreset.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class FillerPreset
+    {
+        public FillerPreset(string resourceName, int count = 10, float localScale = 1, float distance = 1)
+        {
+            ResourceName = resourceName;
+            Distance = distance;
+            Count = count;
+            LocalScale = new Vector3(localScale, localScale, localScale);
+        }
+
+        public string ResourceName { get; private set; }
+        public int Count { get; private set; }
+        public float Distance { get; private set; }
+        public Vector3 LocalScale { get; private set; }
+    }
+}
--- Assets/Respawner.cs
+++ Assets/Respawner.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,31 +7,90 @@ namespace Assets
 {
     public class Respawner : MonoBehaviour
     {
-        private GameObject _etalon;
+        public int CurrentPreset;
+
+        private readonly IList<FillerPreset> _presets;
+        private readonly IList<GameObject> _fillInstances;
+
+        public Respawner()
+        {
+            _presets = new List<FillerPreset>();
+            _fillInstances = new List<GameObject>();
+        }
+
+        public void Awake()
+        {
+            _presets.Add(new FillerPreset("Ball", 1000, 0.4f, 0.2f));
+            _presets.Add(new FillerPreset("Skull", 50, 0.2f, 1.1f));
+        }
 
         public void Start()
         {
-            _etalon = Resources.Load("Ball") as GameObject;
+            if (!_presets.Any())
+                return;
 
-            const float ballScale = 0.4f;
-            const float ballDispersion = 5.4f;
+            SetPreset(0);
+        }
 
-            for (var i = 0; i < 1000; i++)
+        private void SetPreset(FillerPreset preset)
+        {
+            if (preset == null)
             {
-                var newObject = Instantiate(_etalon, Vector3.zero, Quaternion.identity);
+                Debug.LogError("Не удалось выставить презет, так как он null");
+                return;
+            }
 
-                var posX = ballDispersion * Random.value - ballDispersion / 2;
-                var posZ = ballDispersion * Random.value - ballDispersion / 2;
-                var posY = 2 + i / 10 * 2 * ballScale;
+            foreach (var instance in _fillInstances)
+                Destroy(instance);
+
+            var etalon = Resources.Load(preset.ResourceName) as GameObject;
+            if (etalon == null)
+            {
+                Debug.LogError(string.Format("Не удалось найти ресурс {0}", preset.ResourceName));
+                return;
+            }
+
+            const float dispersion = 5.4f;
+
+            for (var i = 0; i < preset.Count; i++)
+            {
+                var newObject = Instantiate(etalon, Vector3.zero, Quaternion.identity);
+
+                var posX = dispersion * Random.value - dispersion / 2;
+                var posZ = dispersion * Random.value - dispersion / 2;
+                var posY = 2 + i / 10 * 2 * preset.Distance;
 
                 newObject.transform.position = new Vector3(posX, posY, posZ);
-                newObject.transform.localScale = new Vector3(ballScale, ballScale, ballScale);
+                newObject.transform.localScale = preset.LocalScale;
+
+                _fillInstances.Add(newObject);
             }
+        }
+
+        private void SetPreset(int presetPos)
+        {
+            if (presetPos >= _presets.Count || CurrentPreset < 0)
+                presetPos = 0;
+
+            var preset = _presets[presetPos];
+            CurrentPreset = presetPos;
+            SetPreset(preset);
         }
 
         public void Update()
         {
-
+            if (Input.GetKeyDown("r"))
+            {
+                SetPreset(CurrentPreset);
+            }
+            if (Input.GetKeyDown("space"))
+            {
+                CurrentPreset++;
+                CurrentPreset = CurrentPreset % _presets.Count;
+                SetPreset(CurrentPreset);
+            }
         }
+
+
     }
 }
